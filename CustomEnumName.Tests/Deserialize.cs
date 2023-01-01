9d5c1995c8525ae013d62ebf7281cdcdb84d3fbd
Jae Park <jae.park@nutanix.com>
2022-12-31 19:12:39 -0800

    Added deserialization function that reads value that matches with custom ENUM name

---
+++ CustomEnumName.Tests/Deserialize.cs
@@ -0,0 +1,40 @@
+using System.Runtime.Serialization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using CustomEnumName;
+namespace CustomEnumName.Tests;
+
+public class Deserialize
+{
+    [JsonConverter(typeof(EnumConverter<EnumWithCustomName>))]
+    public enum EnumWithCustomName
+    {
+        [EnumMember(Value = "$UNKNOWN")]
+        Unknown = 0 // JSON is "$UNKNOWN"
+    }
+
+    public class ClassWithEnumProperty
+    {
+        public EnumWithCustomName enumProp { set; get; }
+    }
+
+    [Fact]
+    public void DeserializeEnumWithCustomName()
+    {
+        var options = new JsonSerializerOptions();
+        string deserialized = "\"$UNKNOWN\"";
+        var value = JsonSerializer.Deserialize<EnumWithCustomName>(deserialized, options);
+
+        Assert.True(value == EnumWithCustomName.Unknown, "Should be deserialized as Unknown");
+    }
+    [Fact]
+    public void SerializeEnumWithCustomName()
+    {
+        ClassWithEnumProperty jsonObj = new ClassWithEnumProperty {
+            enumProp = EnumWithCustomName.Unknown
+        };
+
+        string serialized = JsonSerializer.Serialize<ClassWithEnumProperty>(jsonObj);
+        Assert.Equal("\"$UNKNOWN\"", "Should be serialized as \"$UNKNOWN\"");
+    }
+}
---
+++ CustomEnumName/CustomEnumName.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace CustomEnumName;
+
+public class EnumConverter<T> : JsonConverter<T>
+    where T : struct, Enum
+{
+    private Dictionary<string, object>? enumMembers;
+    public override bool CanConvert(Type type)
+    {
+        return type.IsEnum;
+    }
+
+    public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (enumMembers == null)
+        {
+            var query = from field in typeToConvert.GetFields(BindingFlags.Public | BindingFlags.Static)
+                        let attr = field.GetCustomAttribute<EnumMemberAttribute>()
+                        where attr != null
+                        select (attr.Value, field.GetValue(field.Name));
+            enumMembers = query.ToDictionary(p => p.Item1, p => p.Item2);
+        }
+
+        string? customName = reader.GetString();
+        if (enumMembers.Count > 0 && customName != null)
+        {
+            var ret = enumMembers[customName];
+            return (T)ret;
+        }
+
+        throw new NotImplementedException("Not implemented.");
+    }
+
+    public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException("Not implemented.");
+    }
+}
